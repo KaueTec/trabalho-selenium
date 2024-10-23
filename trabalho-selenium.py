d1967e6e7c0b24c001b216aa507efac34d165daf
Kauê Eduardo <kaue.davila@duque.g12.br>
2024-10-22 21:46:22 -0300

    Segundo commit

--- trabalho-selenium.py
+++ trabalho-selenium.py
@@ -4,3 +4,14 @@ from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
 opts = ChromeOptions()
 opts.add_experimental_option("detach", True)
+navegador = webdriver.Chrome(options = opts)
+
+#
+url1 = r'https://www.magazineluiza.com.br/'
+navegador.get(url1)
+pesquisar_dell = navegador.find_element(By.ID, "input-search")
+pesquisar_dell.send_keys("Dell G15")
+
+#url2
+
+#url3
